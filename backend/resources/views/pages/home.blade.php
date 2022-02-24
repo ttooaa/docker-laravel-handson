6cff81b3763f28e5bbee98caf22b17d3763cfe73
siiba <ttooaassbb@gmail.com>
2022-02-25 02:08:25 +0900

    minタグ完成

--- backend/resources/views/pages/home.blade.php
+++ backend/resources/views/pages/home.blade.php
@@ -3,8 +3,12 @@
 
 @section('content')
 <div class="home-top">
-    <h3>Know the inside</h3>
+    <h3>Know the inside.</h3>
     <h1>新しい『出会い』は人生を楽しくする。</h1>
-    {{-- <img class="main-back" src="{{ asset('images/back-A.jpg') }}" alt="メイン画面"> --}}
+    <div id="cloud-box">
+        <img class="cloud" src="{{ asset('images/cloud.png') }}" alt="雲">
+        <img class="font fontA" src="{{ asset('images/font-A.png') }}" alt="通話から始まる恋">
+        <img class="font fontB" src="{{ asset('images/font-B.png') }}" alt="していきませんか？">
+    <div>
 </div>
 @endsection
