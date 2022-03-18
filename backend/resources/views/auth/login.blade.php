98215cd73a5c44bb7ca85f0f2ba5f76b0358fecb
siiba <ttooaassbb@gmail.com>
2022-03-19 00:31:07 +0900

    自作ログインページでログイン実行可能　jsのonkeyup使用により同時入力を実装 追加修正

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -4,9 +4,8 @@
 @section('head_link')
     <link rel="stylesheet" href="{{ asset('css/loginstyle.css') }}">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    {{-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> --}}
-    {{-- ↑のがあると検証ツールでエラーになる --}}
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 @endsection
 
@@ -15,7 +14,7 @@
         <div class="opacity-1">
             <div class="login-wrap">
                 <div class="login-html">
-                    <form method="POST" action="?">
+                    <form method="POST">
                         @csrf
 
                         @if (session('status'))
