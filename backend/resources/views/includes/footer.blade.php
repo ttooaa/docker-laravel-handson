a89d63231b84672d6f4a3f27a901da3be13eec1a
siiba <ttooaassbb@gmail.com>
2022-03-23 00:24:35 +0900

    ベースとなるcss作成

--- backend/resources/views/includes/footer.blade.php
+++ backend/resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 {{-- フッター --}}
 <a href="#"><img class="footer-logo" src="{{ asset('images/キョウのワダイ.jpeg') }}" alt="toppage"></a>
 
-<p>Copyright © 2021 Toa Shiba</p>
+<p class="footer-p ml-10">Copyright © 2021 Toa Shiba</p>
--- backend/resources/views/includes/header.blade.php
+++ backend/resources/views/includes/header.blade.php
@@ -2,21 +2,21 @@
 <a href="{{ url('/') }}"><img class="header-logo" src="{{ asset('images/キョウのワダイ.jpeg') }}" alt="toppage"></a>
 
 @auth
-    <ul>
-        <li><a href="#">参加する</a></li>
-        <li><a href="#">募集する</a></li>
-        <li><a href="#">ステータス</a></li>
-        <li><a href="{{ url('/my_page') }}">マイページ</a></li>
-        <li><a href="{{ url('/inquiry/input') }}">お問い合わせ</a></li>
+    <ul class="header-ul">
+        <li class="header-li"><a  class="header-a" href="#">参加する</a></li>
+        <li class="header-li"><a  class="header-a" href="#">募集する</a></li>
+        <li class="header-li"><a  class="header-a" href="#">ステータス</a></li>
+        <li class="header-li"><a  class="header-a" href="{{ url('/my_page') }}">マイページ</a></li>
+        <li class="header-li"><a  class="header-a" href="{{ url('/inquiry/input') }}">お問い合わせ</a></li>
     </ul>
 @endauth
 
 @guest
-    <ul>
-        <li><a href="{{ route('login') }}">ログイン</a></li>
+    <ul class="header-ul">
+        <li class="header-li"><a  class="header-a" href="{{ route('login') }}">ログイン</a></li>
         @if (Route::has('register'))
-            <li><a href="{{ route('register') }}">新規登録</a></li>
+        <li class="header-li"><a  class="header-a" href="{{ route('register') }}">新規登録</a></li>
         @endif
-        <li><a href="{{ url('/inquiry/input') }}">お問い合わせ</a></li>
+        <li class="header-li"><a  class="header-a" href="{{ url('/inquiry/input') }}">お問い合わせ</a></li>
     </ul>
 @endguest
