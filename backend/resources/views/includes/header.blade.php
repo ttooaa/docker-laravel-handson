2946a21f790f701d98c7e659e9c766e9eecf2f9e
siiba <ttooaassbb@gmail.com>
2022-03-27 21:15:58 +0900

    base.blade.php編集

--- backend/resources/views/includes/header.blade.php
+++ backend/resources/views/includes/header.blade.php
@@ -3,37 +3,36 @@
 
 @auth
     <ul class="header-ul">
-        <li class="header-li"><a  class="header-a" href="#">参加する</a></li>
-        <li class="header-li"><a  class="header-a" href="#">募集する</a></li>
-        <li class="header-li"><a  class="header-a" href="#">ステータス</a></li>
-        <li class="header-li"><a  class="header-a" href="{{ url('/my_page') }}">マイページ</a></li>
-        {{-- <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }}
+        <li><a  class="nav-link text-dark" href="#">参加する</a></li>
+        <li><a  class="nav-link text-dark" href="#">募集する</a></li>
+        <li><a  class="nav-link text-dark" href="#">ステータス</a></li>
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link text-dark dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}さん
             </a>
-
             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <a  class="dropdown-item" href="{{ url('/my_page') }}">{{ __('プロフィール編集') }}</a>
                 <a class="dropdown-item" href="{{ route('logout') }}"
                    onclick="event.preventDefault();
                                  document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
+                    {{ __('ログアウト') }}
                 </a>
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                     @csrf
                 </form>
             </div>
-        </li> --}}
-        <li class="header-li"><a  class="header-a" href="{{ url('/inquiry/input') }}">お問い合わせ</a></li>
+        </li>
+        <li><a  class="nav-link text-dark" href="{{ url('/inquiry/input') }}">お問い合わせ</a></li>
     </ul>
 @endauth
 
 @guest
     <ul class="header-ul">
-        <li class="header-li"><a  class="header-a" href="{{ route('login') }}">ログイン</a></li>
+        <li><a  class="nav-link text-dark" href="{{ route('login') }}">ログイン</a></li>
         @if (Route::has('register'))
-        <li class="header-li"><a  class="header-a" href="{{ route('register') }}">新規登録</a></li>
+        <li><a  class="nav-link text-dark" href="{{ route('register') }}">新規登録</a></li>
         @endif
-        <li class="header-li"><a  class="header-a" href="{{ url('/inquiry/input') }}">お問い合わせ</a></li>
+        <li><a  class="nav-link text-dark" href="{{ url('/inquiry/input') }}">お問い合わせ</a></li>
     </ul>
 @endguest
