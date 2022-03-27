8c8edc7e2a277b28c33eed6c440544f1b763d55f
siiba <ttooaassbb@gmail.com>
2022-03-27 23:54:04 +0900

    メール再送信verifyページ完成

--- backend/resources/views/auth/verify.blade.php
+++ backend/resources/views/auth/verify.blade.php
@@ -1,28 +1,33 @@
-@extends('layouts.app')
+{{-- ログインページ --}}
+@extends('layouts.base')
+@section('title','キョウのワダイ-ログイン')
+
+@section('head_link')
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    <div id="body">
+        <div class="opacity-veri">
+            <div class="login-wrap-veri">
+                <div class="login-html-veri">
+                    <div class="title-veri h5">{{ __('メールアドレスをご確認ください') }}</div>
 
-                <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('ご登録いただいたメールアドレスに確認用のリンクをお送りしました。') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('メールをご確認ください。') }}
+                    {{ __('もし確認用メールが送信されていない場合は、下記をクリックしてください。') }}
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('確認メールを再送信する') }}</button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
