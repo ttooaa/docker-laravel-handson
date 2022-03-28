f0469164d63411f6aa14733771baae79b6704666
siiba <ttooaassbb@gmail.com>
2022-03-28 22:03:39 +0900

    join画面作成

--- backend/resources/views/user/joijn.blade.php
+++ backend/resources/views/user/joijn.blade.php
@@ -1,5 +1,10 @@
 {{-- 参加画面の表示 --}}
 @extends('layouts.base')
+@section('title','キョウのワダイ-マイページ')
+
+@section('head_link')
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+@endsection
 
 @section('content')
 <p class="inquiry-new-user">ログイン成功！</p>
