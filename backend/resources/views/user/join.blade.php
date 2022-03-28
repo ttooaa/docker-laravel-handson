c4b571128780d62082406880376bbb35751f7808
siiba <ttooaassbb@gmail.com>
2022-03-29 01:12:37 +0900

    join画面作成

--- backend/resources/views/user/join.blade.php
+++ backend/resources/views/user/join.blade.php
@@ -9,15 +9,15 @@
 @section('content')
 <section id="back">
     <div class="container">
-        <div class="row my-4 align-items-center">
-            <div class="col-lg-4">
-                <h1>最近の投稿</h1>
+        <div class="row my-4 px-4 align-items-center">
+            <div class="col-3">
+                <h1 class="bg-primary">最近の投稿</h1>
             </div>
-            <div class="col-lg-4">
-                <h1>最近の投稿</h1>
+            <div class="col-3 offset-3">
+                キーワード検索
             </div>
-            <div class="col-lg-4">
-                <h1>最近の投稿</h1>
+            <div class="col-3">
+                絞り込み
             </div>
         </div>
     </div>
