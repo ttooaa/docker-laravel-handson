3739543a4607f3b10069d8116c89c4a3c12801bf
siiba <ttooaassbb@gmail.com>
2022-03-29 22:28:12 +0900

    join画面 投稿中身作成

--- backend/resources/views/user/join.blade.php
+++ backend/resources/views/user/join.blade.php
@@ -9,16 +9,38 @@
 @section('content')
 <section id="back">
     <div class="container">
-        <div class="row my-4 px-4 align-items-center">
+        {{-- 項目部分 --}}
+        <div class="row my-3 pb-4 align-items-center noto border-bottom border-dark border-3">
             <div class="col-3">
-                <h1 class="bg-primary">最近の投稿</h1>
+                <h1 class="text-center condition">最近の投稿</h1>
             </div>
-            <div class="col-3 offset-3">
-                キーワード検索
+            <div class="offset-3 col-3">
+                <div class="input-group px-2">
+                    <input type="text" class="form-control" placeholder="キーワード入力" aria-label="キーワード入力" aria-describedby="button-addon2">
+                    <button type="button" id="button-addon2" class="btn btn-primary">検索</button>
+                </div>
             </div>
             <div class="col-3">
-                絞り込み
+                <div class="input-group px-2">
+                    <select class="form-select" id="inputGroupSelect02">
+                      <option selected>最近の投稿</option>
+                      <option value="1">フォロワー</option>
+                      <option value="2">まもなく開始</option>
+                      <option value="3">参加人数が多い順</option>
+                      <option value="4">2：2　(男2：女2)</option>
+                      <option value="5">1：1　(男1：女1)</option>
+                      <option value="6">自由枠</option>
+                    </select>
+                    <label for="inputGroupSelect02" class="input-group-text">絞り込み</label>
+                  </div>
             </div>
+        </div>
+
+        {{-- @foreach ($rooms as $room)
+        <div></div>
+        @endforeach --}}
+        <div class="col">
+
         </div>
     </div>
 </section>
