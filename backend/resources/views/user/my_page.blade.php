fa1a3aae502e9640281421f49e8416debc3e6c5e
siiba <ttooaassbb@gmail.com>
2022-04-04 00:56:03 +0900

    プロフィール登録画面作成

--- backend/resources/views/user/my_page.blade.php
+++ backend/resources/views/user/my_page.blade.php
@@ -9,37 +9,39 @@
 
 @section('content')
 <section id="back">
-    <div class="">
-        <h1 class="">まずは簡単な自己紹介を作成しよう</h1>
+    <div class="profileScr">
+        <div  class="profileSize">
+            <h1 class="profileTitle">まずは簡単な自己紹介を作成しよう</h1>
 
-        <form class="inquiry-1 fonts" method="POST" action="">
-            <div class="form-sex">
-                <label class="label-sex dot-icon">性別<span class="surely">必須</span></label>
-                <div class="input-sex">
-                    <input id="man" type="radio" name="sex" value="man"><label for="man">男性</label>
-                    <input id="woman" type="radio" name="sex" value="woman"><label for="woman">女性</label>
-                    <input id="none" type="radio" name="sex" value="none"><label for="none">選択しない</label>
+            <form method="POST" action="">
+                <div class="form-sex">
+                    <label class="label-sex ">性別<span class="surely">必須</span></label>
+                    <div class="input-sex">
+                        <input id="man" type="radio" name="sex" value="man"><label for="man">男性</label>
+                        <input id="woman" type="radio" name="sex" value="woman"><label for="woman">女性</label>
+                        <input id="none" type="radio" name="sex" value="none"><label for="none">選択しない</label>
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-mail">
-                <label class="dot-icon">返信用メールアドレス<span class="surely">必須</span></label>
-                <div class="mail-pass-box">
-                    <p>Eメールアドレス</p>
-                    <input type="email" name="email" >
+                <div class="form-mail">
+                    <label class="">出身地</label>
+                    <div class="mail-pass-box">
+                        <p>Eメールアドレス</p>
+                        <input type="email" name="email" >
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-textarea">
-                <label class="dot-icon">お問い合わせ内容<span class="optional">任意</span></label>
-                <div class="textarea-box">
-                    <textarea name="inquiry" maxlength="600" placeholder="お問い合わせ内容をご記入ください"></textarea>
+                <div class="form-textarea">
+                    <label class="">自己紹介文</label>
+                    <div class="textarea-box">
+                        <textarea name="inquiry" maxlength="600" placeholder="例）お話するのが大好きです。気軽に話せて友達が作りたいと思って始めてみました。どうぞよろしくおねがいします！"></textarea>
+                    </div>
                 </div>
-            </div>
 
-            <button class="button-none form-btn" type="submit">確認へ</button>
-        </form>
-    </div>
+                <button class="button-none form-btn" type="submit">確認へ</button>
+            </form>
+        </div>
+        </div>
 </section>
 
 @endsection
