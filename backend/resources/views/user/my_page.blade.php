b6118812457ff3ea41e8a7c5e2ad396994044b1a
siiba <ttooaassbb@gmail.com>
2022-04-04 21:07:01 +0900

    プロフィール登録画面作成

--- backend/resources/views/user/my_page.blade.php
+++ backend/resources/views/user/my_page.blade.php
@@ -6,6 +6,7 @@
 @endsection
 
 @section('content')
+{{-- <section id="back" style="height:1150px;""> --}}
 <section id="back">
     <div class="formScr">
         <div  class="formSize">
@@ -15,31 +16,32 @@
 
                 {{-- ここに画像登録用のフォームを挿入する --}}
 
-                <div class="form-sex">
-                    <label class="label-sex ">性別<span class="surely">必須</span></label>
-                    <div class="input-sex">
-                        <input id="man" type="radio" name="sex" value="man"><label for="man">男性</label>
-                        <input id="woman" type="radio" name="sex" value="woman"><label for="woman">女性</label>
-                        <input id="none" type="radio" name="sex" value="none"><label for="none">選択しない</label>
+                <div class="form-sex formPb">
+                    <label class="label-sex">性別<span class="surely">必須</span></label>
+                    <div class="input-sex formMl">
+                        <input id="man" type="radio" name="sex" value="man" style="transform:scale(1.5);"><label for="man">男性</label>
+                        <input id="woman" type="radio" name="sex" value="woman" style="transform:scale(1.5);"><label for="woman">女性</label>
+                        <input id="none" type="radio" name="sex" value="none" style="transform:scale(1.5);"><label for="none">選択しない</label>
                     </div>
                 </div>
 
-                <div class="form-mail">
-                    <label class="">出身地</label>
-                    <div class="mail-pass-box">
-                        <p>Eメールアドレス</p>
-                        <input type="email" name="email" >
+                <div class="form-mail mt-3">
+                    <label class="label-sex">出身地<span class="surely">必須</span></label>
+                    <div class="input-sex">
+                        <select name="prefecture" id="">
+                            <option value="1">北海道</option>
+                        </select>
                     </div>
                 </div>
 
                 <div class="form-textarea">
-                    <label class="">自己紹介文</label>
+                    <label class="mb-3">自己紹介文</label>
                     <div class="textarea-box">
-                        <textarea name="inquiry" maxlength="600" placeholder="例）お話するのが大好きです。気軽に話せて友達が作りたいと思って始めてみました。どうぞよろしくおねがいします！"></textarea>
+                        <textarea name="inquiry" maxlength="400" style=height:200px; placeholder="例）お話するのが大好きです。気軽に話せて友達が作りたいと思って始めてみました。どうぞよろしくおねがいします！"></textarea>
                     </div>
                 </div>
 
-                <button class="button-none form-btn" type="submit">確認へ</button>
+                <button class="button-none start-btn formBtn" type="submit">完了</button>
             </form>
         </div>
         </div>
