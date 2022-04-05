abf084c94a6bf42a2df03d141a1f71417df2e354
siiba <ttooaassbb@gmail.com>
2022-04-06 00:40:31 +0900

    募集画面作成

--- backend/resources/views/user/collect.blade.php
+++ backend/resources/views/user/collect.blade.php
@@ -21,7 +21,7 @@
                 <div class="form-mail mt-3">
                     <label class="label-head labelWidth">ジャンル<span class="surely">必須</span></label>
                     <div class="input-head">
-                        <select class="formTextTitle selectWidth" style="width: 300px;" name="genre" id="">
+                        <select class="formTextTitle" style="width: 300px;" name="genre" id="">
                             <option value="1">誰でも可</option>
                         </select>
                     </div>
@@ -30,11 +30,40 @@
                 <div class="form-mail mt-3">
                     <label>時間指定<span class="surely">必須</span></label>
                     <p class="timeCaution">※開始時間で選択した時間が経過後、開催時間が開始されます。</p>
-                    <div class="mail-pass-box">
-                        <label for="startTime" class="label-head my-1" style="font-size: 20px;">開始時間</label>
+                    <div class="timeBox">
+                        <label for="startTime" class="label-head">開始時間</label>
                         <div class="input-head">
-                            <select class="formTextTitle selectWidth" style="width: 300px;" name="genre" id="">
-                                <option value="1">誰でも可</option>
+                            <select id="startTime" class="formTextTitle" style="width: 230px;" name="genre" id="">
+                                <option value="0">すぐ開始</option>
+                                <option value="5">5分後</option>
+                                <option value="10">10分後</option>
+                                <option value="15">15分後</option>
+                                <option value="20">20分後</option>
+                                <option value="25">25分後</option>
+                                <option value="30">30分後</option>
+                                <option value="35">35分後</option>
+                                <option value="40">40分後</option>
+                                <option value="45">45分後</option>
+                                <option value="50">50分後</option>
+                                <option value="55">55分後</option>
+                                <option value="60">1時間後</option>
+                                <option value="75">1時間15分後</option>
+                                <option value="90">1時間30分後</option>
+                                <option value="105">1時間45分後</option>
+                                <option value="120">2時間後</option>
+                            </select>
+                        </div>
+                        <label for="runTime" class="label-head mt-5">開催時間</label>
+                        <div class="input-head">
+                            <select id="runTime" class="formTextTitle mt-5" style="width: 230px;" name="genre" id="">
+                                <option value="15">15分</option>
+                                <option value="30">30分</option>
+                                <option value="45">45分</option>
+                                <option value="60">1時間</option>
+                                <option value="75">1時間15分</option>
+                                <option value="90">1時間30分</option>
+                                <option value="105">1時間45分</option>
+                                <option value="120">2時間</option>
                             </select>
                         </div>
                     </div>
