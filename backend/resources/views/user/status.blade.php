350f36932ed9c33d2cc2fc8a3226d8e9f00b6167
siiba <ttooaassbb@gmail.com>
2022-04-06 21:37:30 +0900

    ステータス作成

--- backend/resources/views/user/status.blade.php
+++ backend/resources/views/user/status.blade.php
@@ -10,66 +10,73 @@
     <div class="container pb-4 border-bottom border-dark border-3">
         {{-- 項目部分 --}}
         <div class="row mt-3 pb-4 align-items-center noto border-bottom border-dark border-3">
-            <div class="col-xl-3 col-lg-4">
-                <h1 class="text-center condition">最近の投稿</h1>
-            </div>
-            <div class="offset-xl-3 col-xl-3 col-lg-4 col-md-6">
-                <div class="input-group px-2">
-                    <input type="text" class="form-control" placeholder="キーワード入力" aria-label="キーワード入力" aria-describedby="button-addon2">
-                    <button type="button" id="button-addon2" class="btn btn-primary">検索</button>
-                </div>
-            </div>
-            <div class="col-xl-3 col-lg-4 col-md-6">
-                <div class="input-group px-2">
-                    <select class="form-select" id="inputGroupSelect02">
-                      <option selected>最近の投稿</option>
-                      <option value="1">フォロワー</option>
-                      <option value="2">まもなく開始</option>
-                      <option value="3">参加人数が多い順</option>
-                      <option value="4">2：2　(男2：女2)</option>
-                      <option value="5">1：1　(男1：女1)</option>
-                      <option value="6">自由枠</option>
-                    </select>
-                    <label for="inputGroupSelect02" class="input-group-text">絞り込み</label>
-                </div>
+            <div class="col-xl-7 col-lg-8">
+                <h1 class="text-center condition">現在の参加 or 募集ステータス</h1>
             </div>
         </div>
 
-        <div class="col-lg-12 mt-4 mx-auto p-4 noto postBack">
+        {{-- 募集車の場合 --}}
+        <div class="col-lg-12 mt-4 mx-auto p-4 noto postBack postCollect">
             <div class="d-flex flex-row">
                 <h2 class="m-0">誰でも可</h2> {{-- 分類 --}}
                 <h2 class="m-0">｜</h2> {{-- 空間 --}}
                 <h2 class="m-0">気軽にお話しましょう！</h2> {{-- title部分 --}}
             </div>
-            {{-- <div class="row mt-3" style="height: 96px;"> --}}
             <div class="row mt-3">
                 <div class="col-xl-6 col-md-8 align-self-center"> {{-- 写真表示 --}}
                     <div class="row">
-                        <img class="col-lg-3 p-0 ms-3 avatar" src="{{ asset('images/丸-中.png') }}" alt="host">
-                        <img class="col-lg-3 p-0 ms-2 avatar" src="{{ asset('images/丸-中.png') }}" alt="host">
-                        <img class="col-lg-3 p-0 ms-2 avatar" src="{{ asset('images/丸-中.png') }}" alt="host">
-                        <img class="col-lg-3 p-0 ms-2 avatar" src="{{ asset('images/丸-中.png') }}" alt="host">
+                        <img class="col-lg-3 p-0 ms-3 avatar" src="{{ asset('images/人物-A.jpg') }}" alt="host">
+                        <img class="col-lg-3 p-0 ms-2 avatar" src="{{ asset('images/人物-A.jpg') }}" alt="host">
+                        <img class="col-lg-3 p-0 ms-2 avatar" src="{{ asset('images/人物-A.jpg') }}" alt="host">
+                        <img class="col-lg-3 p-0 ms-2 avatar" src="{{ asset('images/人物-A.jpg') }}" alt="host">
                     </div>
                 </div>
-                <div class="col-xl-6 col-md-4 align-self-center"> {{-- 詳細分 150文字以内 --}}
-                    <p class="my-3">１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６</p>
+                <div class="col-xl-6 col-md-4 align-self-center text-center"> {{-- 詳細分 150文字以内 --}}
+                    <h4 class="my-3 fw-bold">開始時間　21:30</h4>
+                    <h4 class="my-3 fw-bold">終了時間　22:00</h4>
                 </div>
             </div>
             <div class="row align-item-center mt-3">
-                <div class="col-3"> {{-- 詳細分 --}}
-                    <h5 class="m-0">はじめまして</h5>
+                <div class="col-6 text-center"> {{-- 詳細分 --}}
+                    <h5 class="m-0 fw-bold">参加可能人数：あと3人</h5>
                 </div>
-                <div class="col-3"> {{-- 詳細分 --}}
-                    <h5 class="m-0">はじめまして</h5>
-                </div>
-                <div class="col-3"> {{-- 詳細分 --}}
-                    <h5 class="m-0">はじめまして</h5>
-                </div>
-                <div class="col-3 text-end"> {{-- 詳細分 --}}
-                    <h5 class="m-0">はじめまして</h5>
+                <div class="col-6 text-center"> {{-- 詳細分 --}}
+                    <h5 class="m-0 fw-bold">あなたは募集者です</h5>
                 </div>
             </div>
         </div>
+
+        {{-- 参加者の場合 --}}
+        <div class="col-lg-12 mt-4 mx-auto p-4 noto postBack postCollect">
+            <div class="d-flex flex-row">
+                <h2 class="m-0">誰でも可</h2> {{-- 分類 --}}
+                <h2 class="m-0">｜</h2> {{-- 空間 --}}
+                <h2 class="m-0">気軽にお話しましょう！</h2> {{-- title部分 --}}
+            </div>
+            <div class="row mt-3">
+                <div class="col-xl-6 col-md-8 align-self-center"> {{-- 写真表示 --}}
+                    <div class="row">
+                        <img class="col-lg-3 p-0 ms-3 avatar" src="{{ asset('images/人物-A.jpg') }}" alt="host">
+                        <img class="col-lg-3 p-0 ms-2 avatar" src="{{ asset('images/人物-A.jpg') }}" alt="host">
+                        <img class="col-lg-3 p-0 ms-2 avatar" src="{{ asset('images/人物-A.jpg') }}" alt="host">
+                        <img class="col-lg-3 p-0 ms-2 avatar" src="{{ asset('images/人物-A.jpg') }}" alt="host">
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-4 align-self-center text-center"> {{-- 詳細分 150文字以内 --}}
+                    <h4 class="my-3 fw-bold">開始時間　21:30</h4>
+                    <h4 class="my-3 fw-bold">終了時間　22:00</h4>
+                </div>
+            </div>
+            <div class="row align-item-center mt-3">
+                <div class="col-6 text-center"> {{-- 詳細分 --}}
+                    <h5 class="m-0 fw-bold">参加可能人数：あと3人</h5>
+                </div>
+                <div class="col-6 text-center"> {{-- 詳細分 --}}
+                    <h5 class="m-0 fw-bold">あなたは募集者です</h5>
+                </div>
+            </div>
+        </div>
+
     </div>
 </section>
 @endsection
