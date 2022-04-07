da023e93962b61136e5bb86301728f4c106f723b
siiba <ttooaassbb@gmail.com>
2022-04-08 00:00:45 +0900

    seeder編集

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -8,8 +8,9 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(PrefectureTableSeeder::class);
+        // $this->call(PrefectureTableSeeder::class);
         // \App\Models\User::factory(10)->create();
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        \App\Models\User::truncate();
     }
 }
--- backend/database/seeders/UsersTableSeeder.php
+++ backend/database/seeders/UsersTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,16 +17,31 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert(
+        DB::table('users')->insert([
         [
             'prefecture_id' => '1',
             'email' => 'exit@exit.com',
-            'name' => 'tanaka',
+            'name' => 'tanakaa',
             'password' => '321tanaka',
             'sex' => '1',
             'hobby' => '釣り',
             'profile' => '細身低身長',
-            'image' => 'http//:tanaka.co.jp'
+            'image' => 'http//:tanaka.co.jp',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ],
+        [
+            'prefecture_id' => '2',
+            'name' => Str::random(5),
+            'email' => Str::random(10).'@exit.com',
+            'password' => Hash::make('password'),
+            'sex' => '1',
+            'hobby' => '釣り',
+            'profile' => '細身低身長',
+            'image' => 'http//:tanaka.co.jp',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]
         ]);
     }
 }
