88ed85febb96d2aee7302b1af710b4ac59782c8d
siiba <ttooaassbb@gmail.com>
2022-04-09 18:59:52 +0900

    UserController編集

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+    }
 
     public function __construct()
     {
