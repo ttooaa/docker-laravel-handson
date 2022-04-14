bdd58603cac3edf6fdd95639100d561bc3c0ab31
siiba <ttooaassbb@gmail.com>
2022-04-14 23:25:42 +0900

    profile.blade.phpに年齢と出身地表示

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -23,7 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        // return view('application.post_list');
         return view('/application.join');
     }
 }
