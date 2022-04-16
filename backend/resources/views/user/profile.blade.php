9e74580a8799abee15f092a7b6204c7fe316e1dc
siiba <ttooaassbb@gmail.com>
2022-04-16 18:56:00 +0900

    join.blade.php編集

--- backend/resources/views/user/profile.blade.php
+++ backend/resources/views/user/profile.blade.php
@@ -31,12 +31,28 @@
                 <div class="form-head formPb formMt">
                     <label class="label-head">性別<span class="surely">必須</span></label>
                     <div class="input-head formMl">
-                        <input id="man" type="radio" name="sex" value="man" style="transform:scale(1.5);"><label for="man">男性</label>
-                        <input id="woman" type="radio" name="sex" value="woman" style="transform:scale(1.5);"><label for="woman">女性</label>
-                        <input id="none" type="radio" name="sex" value="none" style="transform:scale(1.5);" checked><label for="none">選択しない</label>
+                        @empty ($user->sex)
+                            <input id="man" type="radio" name="sex" value="man" style="transform:scale(1.5);"><label for="man">男性</label>
+                            <input id="woman" type="radio" name="sex" value="woman" style="transform:scale(1.5);"><label for="woman">女性</label>
+                            <input id="none" type="radio" name="sex" value="none" style="transform:scale(1.5);" checked><label for="none">選択しない</label>
+                        @else
+                            @if ($user->sex === 'man')
+                                <input id="man" type="radio" name="sex" value="man" style="transform:scale(1.5);"><label for="man" selected>男性</label>
+                                <input id="woman" type="radio" name="sex" value="woman" style="transform:scale(1.5);"><label for="woman">女性</label>
+                                <input id="none" type="radio" name="sex" value="none" style="transform:scale(1.5);" checked><label for="none">選択しない</label>
+                            @elseif ($user->sex === 'woman')
+                                <input id="man" type="radio" name="sex" value="man" style="transform:scale(1.5);"><label for="man">男性</label>
+                                <input id="woman" type="radio" name="sex" value="woman" style="transform:scale(1.5);"><label for="woman" selected>女性</label>
+                                <input id="none" type="radio" name="sex" value="none" style="transform:scale(1.5);" checked><label for="none">選択しない</label>
+                            @else
+                                <input id="man" type="radio" name="sex" value="man" style="transform:scale(1.5);"><label for="man">男性</label>
+                                <input id="woman" type="radio" name="sex" value="woman" style="transform:scale(1.5);"><label for="woman">女性</label>
+                                <input id="none" type="radio" name="sex" value="none" style="transform:scale(1.5);" checked><label for="none" selected>選択しない</label>
+                            @endif
+                        @endempty
                     </div>
                 </div>
-
+                
                 <div class="form-mail mt-3">
                     <label class="label-head">年齢<span class="surely">必須　</span></label>
                     <div class="input-head">
@@ -89,7 +105,11 @@
                 <div class="form-textarea">
                     <label class="mb-3">自己紹介文</label>
                     <div class="textarea-box">
-                        <textarea name="inquiry" maxlength="400" style=height:200px; placeholder="例）お話するのが大好きです。気軽に話せる友達が作りたいと思って始めてみました。どうぞよろしくおねがいします！"></textarea>
+                        @empty ($user->profile)
+                            <textarea name="inquiry" maxlength="400" style=height:200px; placeholder="例）お話するのが大好きです。気軽に話せる友達が作りたいと思って始めてみました。どうぞよろしくおねがいします！"></textarea>
+                        @else
+                            <textarea name="inquiry" maxlength="400" style=height:200px; placeholder="例）お話するのが大好きです。気軽に話せる友達が作りたいと思って始めてみました。どうぞよろしくおねがいします！">{{ $user->profile }}</textarea>
+                        @endempty
                     </div>
                 </div>
 
