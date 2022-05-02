3ec37a9a6afddf02ad2c650722ba3fe1c4684651
siiba <ttooaassbb@gmail.com>
2022-05-02 12:55:45 +0900

    UserControllerの編集

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -30,7 +30,6 @@ Route::middleware(['verified'])->group(function(){
         return view('user.status');
     });
     //メール認証ができたユーザーのみ実行できるルート
-    //例えば今後機能を実装していく際にメール認証を終えていないユーザーが見えないようにする機能が欲しい場合にここに記述する
 });
 
 // Route::get('/join', [App\Http\Controllers\HomeController::class, 'index'])->name('join');
