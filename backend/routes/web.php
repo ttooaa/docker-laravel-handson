1d5def87ba9c51b3cebb3d9c7adbd6a57509bf24
siiba <ttooaassbb@gmail.com>
2022-05-18 23:16:09 +0900

    collect.blade.phpの編集

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes(['verify' => true]);
 
-
 Route::middleware(['verified'])->group(function(){
     Route::resource('user', UserController::class);
     // Route::get('profile', [UserController::class, 'edit'])->name('profile');
